566b946a159e826f0ca789679b8b8190ed79e620
ShayCohenn <shay91847@gmail.com>
2024-04-21 14:12:07 +0300

    commit 2

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 import os
-import vonage
+from typing import Final
+from vonage import Client, Sms
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET = os.getenv('SMS_SECRET')
-SMS_KEY = os.getenv('SMS_KEY')
+SECRET: Final[str] = os.getenv('SMS_SECRET')
+SMS_KEY: Final[str] = os.getenv('SMS_KEY')
+FROM_USER: Final[str] = "sms test"
+TO_USER: Final[str] = "972528512751"
 
-FROM_USER = "sms test"
-TO_USER = "972500000000"
+def main() -> None:
+    client: Client = Client(key=SMS_KEY, secret=SECRET)
+    sms: Sms = Sms(client)
 
-client = vonage.Client(key=SMS_KEY, secret=SECRET)
-sms = vonage.Sms(client)
+    with open('message.txt', 'r', encoding='utf-8') as f:
+        text: str = f.read()
 
-with open('message.txt', 'r', encoding='utf-8') as f:
-    text = f.read()
+    responseData = sms.send_message(
+        {
+            "from": FROM_USER,
+            "to": TO_USER,
+            "text": text,
+            'type':'unicode'
+        }
+    )
 
-responseData = sms.send_message(
-    {
-        "from": FROM_USER,
-        "to": TO_USER,
-        "text": text,
-        'type':'unicode'
-    }
-)
+    if responseData["messages"][0]["status"] == "0":
+        print("Message sent successfully.")
+    else:
+        print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
 
-if responseData["messages"][0]["status"] == "0":
-    print("Message sent successfully.")
-else:
-    print(f"Message failed with error: {responseData['messages'][0]['error-text']}")
+if __name__ == "__main__":
+    main()
